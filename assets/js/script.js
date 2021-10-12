2b773f230925e6e6b3b346d3c0a1e7c005d4f0c9
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-12 16:47:24 -0400

    Added more HTML elements for submitting score and viewing high score list.

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Declaring variables
+var startingBoxEl = document.getElementById('startingBox');
+var quizBoxEl = document.getElementById('quizBox');
+var questionEl = document.getElementById('question');
+var answerEl = document.getElementById('answers');
+var resultEl = document.getElementById('result');
+var startButton = document.getElementById('startQuiz');
+
+var currentQuestion = 0;
+var score = 0;
+
+// Declaring question set as an array within an object
+var questionSet = [
+    {
+        question: 'Which of the following operators means "and"?',
+        answers: {
+            a: '>=',
+            b: '!',
+            c: '||',
+            d: '&&'
+        },
+        correctAnswer: 'd'
+    },
+]
+
+// Declaring functions
+function startQuiz()
+
+function showQuestions() {
+
+}
+
+function showResults()
